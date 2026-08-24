aa84eb01f60f1e05ed2a9369c89f49e4f951946c
SHEMNDARONGUGI <ndaroshem@gmail.com>
2026-08-24 22:52:38 +0300

    Updated Experience Schema

---
+++ server/src/features/experience/exp.schema.ts
@@ -0,0 +1,17 @@
+import { z } from "zod";
+
+export const createExperienceSchema = z.object({
+  company: z.string().min(1, "Company is required"),
+  role: z.string().min(1, "Role is required"),
+  description: z.string().min(1, "Destription is required"),
+  startDate: z.string().min(1, "Start date is required"),
+  endDate: z.string().optional(),
+  technologies: z.array(z.string()).default([]),
+  current: z.boolean().default(false),
+});
+
+export const updateExperienceSchema = createExperienceSchema.partial();
+
+export type CreateExperienceData = z.infer<typeof createExperienceSchema>;
+
+export type UpdateExperienceData = z.infer<typeof updateExperienceSchema>;
